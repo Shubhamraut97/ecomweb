cb8a7a19ef557137c67726d356a745b590a82f85
shubham raut <shubhamraut0097@gmail.com>
2025-12-15 11:22:03 +0545

    changes made in th ui

--- ecom/views.py
+++ ecom/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from category.models import Category
 from store.models import Product
 
 
 def home(request):
-    products=Product.objects.all().filter(is_available=True)
-    context={
-        'products':products
+    products = Product.objects.all().filter(is_available=True).order_by('-created_date')[:8]  # Latest 8 products
+    categories = Category.objects.all()[:6]  # Show 6 categories
+    context = {
+        'products': products,
+        'categories': categories,
     }
     return render(request, "home.html", context)
 # Create your views here.
